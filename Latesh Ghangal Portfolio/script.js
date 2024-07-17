0fbd14180fbeb9bf75dbc6f8aa7c56a6522aae6a
itsLatesh <ghangallatesh2001@gmail.com>
2024-07-17 22:27:18 +0530

    C:\Users\latesh ghangal\Desktop\My

---
+++ Latesh Ghangal Portfolio/script.js
@@ -0,0 +1,8 @@
+$('.Project-button').mousedown(function(){
+    timeout = setInterval(function(){
+        window.scrollBy(0,-1); // May need to be -1 to go down
+    }, 0); // Play around with this number. May go too fast
+
+    return false;
+});
+
